293bfe2eaf72c4a1bf7e86d153967bf5264bddbf
GEC-알스톰 <gec_thyristor_chopper@[127.0.0.1]>
2026-06-28 11:57:42 +0900

    gradle 수정

--- gdxhelper/build.gradle.kts
+++ gdxhelper/build.gradle.kts
@@ -1,12 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
+	kotlin("jvm") version "1.9.22"
 }
 
 dependencies {
-    api("com.badlogicgames.gdx:gdx:1.12.1")
+	api("com.badlogicgames.gdx:gdx:1.12.1")
 	implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.22")
-}
-
-tasks.withType<JavaCompile>().configureEach {
-    options.encoding = "UTF-8"
 }
