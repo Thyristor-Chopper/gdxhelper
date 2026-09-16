d5a96af8797a94882e1ac22d8613b91358be27e1
싸이리스터 초퍼제어 <gec_thyristor_chopper@[127.0.0.9]>
2026-09-16 10:00:12 +0900

    까먹음

--- src/main/java/io/potatogun/gdxhelper/screen/Screen.kt
+++ src/main/java/io/potatogun/gdxhelper/screen/Screen.kt
@@ -176,6 +176,7 @@ abstract class Screen(font: BitmapFont = BitmapFont(), _dummy: Nothing? = null) 
 
 	// gdxhelper 내의 Game 객체가 아닌 LibGDX 자체의 Game 객체에서 사용할 때 대비
 	final override fun render(delta: Float) {
+		update(delta);
 		render();
 	}
 
